9e0d367bc0bff350053a428bf1fb65d67d5c8972
Nguyen Xuan Tien Dung <>
2023-10-17 11:54:19 +0700

    no message

---
+++ CinemaBook/Scence/Manager/Management/MangamentBill/ManagementBillTicket/viewModel/ManagementBillProductViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ManagementBillProductViewModel.swift
+//  CinemaBook
+//
+//  Created by Nguyen Xuan Tien Dung on 17/10/2023.
+//  Copyright © 2023 dungtien. All rights reserved.
+//
+
+import UIKit
+
+class ManagementBillProductViewModel: NSObject {
+
+}
